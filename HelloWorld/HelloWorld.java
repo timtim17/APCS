a84a15582b1eb6ca8df8bf903a223721ec6cdc69
Austin Jenchi <timtim17@users.noreply.github.com>
2016-09-17 19:04:32 -0700

    Add HelloWorld projects

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ * 
+ * @author Austin Jenchi (timtim17)
+ * @since 09/12/2016
+ */
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
